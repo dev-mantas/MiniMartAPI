6d47bc767284fb4907417643660a361423033fe4
mantas <mantanascode@gmail.com>
2023-03-20 18:44:29 +0000

    fixed type bugs

--- src/services/user/user.services.ts
+++ src/services/user/user.services.ts
@@ -9,10 +9,12 @@ dotenv.config()
 
 export async function registerUser (req: Request, res: Response) {
     const { email, first_name, last_name } = req.body
+    if(!email || !first_name || !last_name || !req.body.password) return res.status(400).send('MISSING_FIELDS')
     let hashedPassword;
     if(req.body.password) {
         hashedPassword = await bcrypt.hash(req.body.password, 8)     
     }
+    console.log(hashedPassword)
 
     try {
         const result = await userSchema.create({
@@ -30,9 +32,13 @@ export async function registerUser (req: Request, res: Response) {
             }
             // #TODO
             // Send email here
-            return res.status(200).send('ACCOUNT_CREATED')
+            return res.status(200).send({message: 'ACCOUNT_CREATED'})
         })
-    } catch(error) {
+    } catch(error: any) {
+        if(error.code === 11000) {
+            throw res.status(400).send({message: 'DUPLICATE ACCOUNT'})
+        }
+        console.log(error)
         throw res.status(400).send(error)
     }
 }
